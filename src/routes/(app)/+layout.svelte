fa5f9f0c84da7ab5e5a2135c1bda1054a02db5e5
betoab728 <betoab728@gmail.com>
2026-01-07 17:21:20 -0500

    login protect

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -7,12 +7,15 @@
     import { browser } from '$app/environment';
 
 	let { children } = $props();
+    let isAuthorized = $state(false);
 
     onMount(() => {
         if (browser) {
             const token = localStorage.getItem('token');
             if (!token) {
                 goto('/login');
+            } else {
+                isAuthorized = true;
             }
         }
     });
@@ -22,9 +25,16 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+{#if isAuthorized}
 <div class="min-h-screen bg-gray-50 flex">
 	<Sidebar />
 	<main class="flex-1 md:ml-64 min-h-screen flex flex-col transition-all duration-300 w-full">
 		{@render children()}
 	</main>
 </div>
+{:else}
+    <!-- Optional: Add a loading spinner here -->
+    <div class="min-h-screen flex items-center justify-center">
+        <p class="text-gray-500">Verificando sesión...</p>
+    </div>
+{/if}
